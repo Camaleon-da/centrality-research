0402b930980898772a84e9d7e37426e7aa04c477
RAMID138 <diego.x.ramirez.-nd@disney.com>
2021-01-06 18:32:05 -0500

    Fixed terrible php code v2

--- public/vendor/js/contact_us.js
+++ public/vendor/js/contact_us.js
@@ -27,9 +27,9 @@ $(".contact_btn").on('click', function () {
 
         var accessURL;
         if(secondLevelLocation){
-            accessURL="../vendor/contact-mailer.php";
+            accessURL="contact-mailer.php";
         }else{
-            accessURL="vendor/contact-mailer.php";
+            accessURL="contact-mailer.php";
         }
         //data to be sent to server
         $.ajax({
